a1b316fadab4388090e66aa9b7924bafa45bd48d
rishabh-malhotra <rishabhmalhotra110796@gmail.com>
2018-10-18 19:39:39 +0530

    added some routing changes and functionality to show delete comment or article only when the user itself is author

--- src/app/display-article/display-article.component.ts
+++ src/app/display-article/display-article.component.ts
@@ -13,9 +13,13 @@ export class DisplayArticleComponent implements OnInit {
 public selected: object;
 slug: string;
 comments:Array<Object>;
+public username:string;
+public token: string;
+public match:boolean;
   constructor(private router: ActivatedRoute, private getData: DisplayServiceService,private route: Router) { }
 
   ngOnInit() {
+    this.match=false;
     this.router.paramMap.subscribe(params => {
       this.slug = params.get("slug");
       this.getData.getArticleDetails(this.slug).subscribe((status: Object )=> {
@@ -30,6 +34,13 @@ comments:Array<Object>;
   
   saveData(data){
     this.selected=data;
+    this.token = localStorage.getItem('Token');
+    this.username = localStorage.getItem('username');
+    if(this.token){
+      if(this.username===data.article.author.username){
+         this.match=true;
+      }
+  }
   }
   saveComments(data){
     this.comments=data;
@@ -38,18 +49,26 @@ comments:Array<Object>;
     this.getData.postComment(comment.value,this.slug).subscribe((status: Object )=> {console.log(status)});
     this.getData.getAllComments(this.slug).subscribe((status: Array<Object>)=>{
     this.saveComments(status);
+    this.route.navigate([`New-Article/articles/${this.slug}`]);
     });
   }
   deleteComment(id){
     this.getData.removeComment(id,this.slug).subscribe((status:Object)=>{});
     this.getData.getAllComments(this.slug).subscribe((status: Array<Object>)=>{
       this.saveComments(status);
+      this.route.navigate([`New-Article/articles/${this.slug}`]);
       });
   }
   deleteArticle(){
     this.getData.removeArticle(this.slug).subscribe((status:Object)=>{});
     this.goToHome();
     
+  }
+  callSignin(){
+    this.route.navigate(["Sign-In"]);
+  }
+  callSignup(){
+    this.route.navigate(["Sign-Up"]);
   }
   goToHome(){
   this.route.navigate(['Home']);
